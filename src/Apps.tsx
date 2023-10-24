706f8543b29a153c21e969d61d73f96be7bfd919
IngaMuse <ingamoshkareva@gmail.com>
2023-10-25 00:23:58 +0300

    init: start task

--- src/Apps.tsx
+++ src/Apps.tsx
@@ -1,11 +1,11 @@
 function Apps() {
-  const app = function name(params: type) {
-    console.log(123);
-  };
-  app(1);
+  // const app = function name(params: type) {
+  //   console.log(123);
+  // };
+  // app(1);
   return <div>Hello World
 
-    
+
   </div>;
 }
 
