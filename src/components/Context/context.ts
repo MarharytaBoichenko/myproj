4c002d6cba5794a626f9a638e576fa4af1d330fd
MarharytaBoichenko <boichenkomv@gmail.com>
2023-11-15 20:14:48 +0200

    feat:

--- src/components/Context/context.ts
+++ src/components/Context/context.ts
@@ -2,6 +2,7 @@ import { createContext } from "react";
 import { ICard } from "../../types/card.types";
 
 const search = localStorage.getItem("search") || "";
+console.log("first");
 
 type ISearchContext = [string, React.Dispatch<React.SetStateAction<string>>];
 export const SearchContext = createContext<ISearchContext>([
